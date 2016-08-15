c1b557f8910d2aacf9f86a1aaf17a8920b92b4ec
Vsevolod <dramdoobas123456@gmail.com>
2016-08-15 18:33:13 +0400

    end

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,17 @@
 $(document).ready(function(){
-		var header_menu_name = 'header-menu',
-			header_menu = $('.' + header_menu_name),
-			navbar_toggle = $('.navbar-toggle');
-		function toggler(){
-			navbar_toggle.toggleClass('navbar-toggle-open');
-			header_menu.toggleClass('header-menu-open');
-		}
-		navbar_toggle.click(function(e){
-			toggler();
-			e.stopPropagation();
+		$(".modal-close").click(function(){
+			$(this).closest(".modal-bg").toggleClass("dn");
+			$("body").removeClass("modal-open");
+			$(".modal-bg").removeClass("thismodalopen");
 		});
-		$(document).click(function(e){
-			if( header_menu.hasClass(header_menu_name + '-open') ) {
-				if ( ! $(e.target).is('.'+header_menu_name + ', .'+header_menu_name+"*") ) {
-					toggler();
-				}
-			}
+		$(".modal-bd").click(function(){
+			$(this).parent().toggleClass("dn");
+			$("body").removeClass("modal-open");
+			$(".modal-bg").removeClass("thismodalopen");
 		});
-		$('.anchor').click(function(){
-			var href = $(this).attr('href');
-			$('body,html').animate({
-				scrollTop: $(href).offset().top
-			},2000);
-			return false;
+		$("#politika-link").click(function(){
+			$("#modal-politika").toggleClass("dn thismodalopen");
+			$("body").toggleClass("modal-open");
 		});
-		$(window).on('hashchange', function(e){
-			history.replaceState ("", document.title, e.originalEvent.oldURL);
-		});
+		
 });	
